afff3e2ca1a19cdabfa24bdff77c67fa71ee1907
Диана Ильина <diana.1993.30@gmail.com>
2022-03-12 17:38:13 +0000

    Fixing spaces

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int startBalance = 100;
         int topUp = 2000;
-        int bonusRuble = topUp>=1000 ? topUp/100 : 0;
+        int bonusRuble = topUp >= 1000 ? topUp / 100 : 0;
 
         System.out.println("Бонусные рубли: " + bonusRuble);
     }
